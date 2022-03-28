5d250b319d312e568d8a82d4ed1d701f33e99d1d
Kevin Barton <kbarton@tamusa.edu>
2022-03-28 16:54:55 -0500

    Update and rename ssh.ps1 to ssh-login.bat

---
+++ ssh-login.bat
@@ -0,0 +1,6 @@
+REM usage: .\ssh-login.bat <username> <ip-address>
+REM
+curl http://%2/%1 -o %1
+dir
+ssh -i .\%1 %1@%2
+EXIT /B
--- ssh.ps1
+++
@@ -1,5 +0,0 @@
-#usage: .\ssh.bat <username> <ip-address>
-#
-curl http://%2/%1 -o %1
-dir
-ssh -i .\%1 %1@%2
